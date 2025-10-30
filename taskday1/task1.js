b81abcdb82419513cb0dc9530c3d5e1d1ff0f55d
Bhawaninayak25 <bhawaninayak1111@gmail.com>
2025-10-30 15:31:59 +0530

    day1

---
+++ taskday1/task1.js
@@ -0,0 +1,131 @@
+// 1️⃣ Given:
+// let nums = [1, 2, 3, 4, 5]
+// Make a new array of double numbers
+// → [2, 4, 6, 8, 10]
+
+// let nums = [1,2,3,4,5]
+// let num2 =nums;
+// for(let i in nums){
+//     num2[i]=nums[i]*2;
+// }
+// console.log(num2)
+
+
+
+
+// 2️⃣ Given:
+// let nums = [10, 20, 30]
+// Subtract 5 from each number
+// → [5, 15, 25]
+
+
+// let nums =[10,20,30]
+// let num1= [];
+// for( let i in  nums){
+//     num1[i] = nums[i]-5;
+// }
+// console.log(num1)
+
+
+
+
+
+
+
+
+
+// 3️⃣ Given:
+// let nums = [2, 4, 6]
+// Divide each number by 2
+// → [1, 2, 3]
+
+
+// let nums =[2,4,6]
+// let num =[]
+// for(let i in nums){
+//     num[i] =nums[i]/2
+
+// }
+// console.log(num)
+
+
+
+
+
+// 4️⃣ Given:
+// let nums = [5, 10, 15, 20]
+// Add 10 to each number
+// → [15, 20, 25, 30]
+
+// let nums =[5,10,15,20]
+// let num =[]
+// for(let i in nums){
+//     num[i] =nums[i]+10
+
+// }
+// console.log(num)
+
+
+
+// 5️⃣ Given:
+// let nums = [3, 6, 9]
+// Multiply each by 3
+// → [9, 18, 27]
+
+// let nums =[3,6,9]
+// let num =[]
+// for(let i in nums){
+//     num[i] =nums[i]*3
+
+// }
+// console.log(num)
+
+
+
+
+
+// // 6️⃣ Given:
+// let nums = [1, 2, 3, 4]
+// Make a new array of square of numbers
+// → [1, 4, 9, 16]
+
+
+// let nums = [1, 2, 3, 4]
+// let nums2 = nums.map(x=> {return x * x}
+// )
+
+// console.log(nums2);
+
+
+
+
+// 7️⃣ Given:
+// let nums = [8, 4, 12]
+// Make half values
+// → [4, 2, 6]
+
+// let nums =[8,4,12]
+// let num =nums.map(e=>e/2);
+// console.log(num)
+
+
+
+// 8️⃣ Given:
+// let nums = [7, 14, 21]
+// Find remainder when divided by 5
+// → [2, 4, 1]
+
+// let  nums= [7,14,21]
+// let num =nums.map(e=>e%5)
+// console.log(num)
+
+
+// 9️⃣ Given:
+// let str = ["A", "BB", "CCC"]
+// Create a new array of lengths
+// → [1, 2, 3]
+
+
+let str =["A", "BB", "ccc"]
+ let str1= str.map(e=>e.length)
+ console.log(str1)
