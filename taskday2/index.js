1088bdd4ebf32b5270c808c26db2f747d34d461e
Bhawaninayak25 <bhawaninayak1111@gmail.com>
2025-11-05 15:11:49 +0530

    a

---
+++ taskday2/index.js
@@ -0,0 +1,68 @@
+// 
+
+
+
+// let prices = [100, 200, 300];
+// let finalPrices = prices.map(p => p + p * 0.1);
+// console.log(finalPrices);
+
+
+// let words = ["one", "two", "three"];
+// let upperWords = words.map(w => w.toUpperCase());
+// console.log(upperWords);
+
+
+
+// let tempsC = [0, 25, 37, 100];
+// let tempsF = tempsC.map(c => c * 1.8 + 32);
+// console.log(tempsF);
+
+
+
+// let marks = [20, 45, 70, 30];
+// let result = marks.map(m => (m >= 35 ? "Pass" : "Fail"));
+// console.log(result);
+
+
+
+// let nums = [10, 25, 40, 3, 81];
+// let even = nums.filter(n => n % 2 === 0);
+// console.log(even);
+
+// let ages = [12, 18, 22, 16, 30];
+// let adults = ages.filter(a => a >= 18);
+// console.log(adults);
+
+
+
+
+// let words = ["one", "four", "sixteen", "two"];
+// let longWords = words.filter(w => w.length > 3);
+// console.log(longWords);
+
+
+
+// let marks = [45, 60, 30, 90, 50];
+// let highMarks = marks.filter(m => m > 50);
+// console.log(highMarks);
+
+
+
+// let prices = [50, 200, 100, 75, 120];
+// let affordable = prices.filter(p => p <= 100);
+// console.log(affordable);
+
+
+
+
+
+// let nums = [1, 2, 3, 4];
+// nums.forEach(n => console.log(n * 3));
+
+
+// let names = ["rajesh sir ", "Sara", "Raj"];
+// names.forEach(n => console.log(`hello ${n}`))
+
+let marks = [45, 67, 80];
+
+
