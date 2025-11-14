e92e61d9ea4825cbcb7e531df3e74e767675e711
Bhawaninayak25 <bhawaninayak1111@gmail.com>
2025-11-14 15:32:25 +0530

    day12

---
+++ day11/index.js
@@ -0,0 +1,28 @@
+// 🧩 Task 1 — Filter passing students
+// Concepts: filter(), >=, if
+// const marks = [45, 67, 89, 23, 50, 90];
+
+// let marks=[45,67,89,23,50,90]
+// let val =marks.filter(val=>val>=60)
+//     console.log(val)
+
+
+// const students = [
+//   { name: "Amit", marks: 35 },
+//   { name: "Priya", marks: 70 },
+//   { name: "Ravi", marks: 48 },
+//   { name: "Neha", marks: 80 }
+// ];
+// let c =students.filter(e=> e.marks<=50)
+//     console.log(c)
+
+
+
+// 🧩 Task 3 — Check equality of two arrays (length check only)
+// Concepts: ==, ===, if
+// const arr1 = [10, 20, 30];
+// const arr2 = [10, 20, 30];
+// let aray=arr1.filter(e=> arr1==arr2)
+// console.log(aray)
+
+
---
+++ day12/index.js
@@ -0,0 +1,29 @@
+// . Task: Check if a number is between 10 and 20.
+
+// Input: 15
+
+// Expected Output: “Number is between 10 and 20”
+
+
+
+
+// let num=15;
+// if(num>10 && num<20){
+//     console.log("number is beetwin 10 and 20  is =" , num)
+// }
+
+
+
+// 2. Task: Check if a person is eligible to vote and has an ID.
+
+// Input: age = 19, hasID = true
+
+// Expected Output: “You can vote”
+
+
+// let hasID;
+// if(hasId=true){
+//     console.log("you can vote ")
+// }if(hasId=false){
+//     console.log("you cant vote")
+// }
