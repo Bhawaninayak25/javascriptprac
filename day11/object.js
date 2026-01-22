810d113f446d7cf4bcd5a80285e493bdf73d713f
Bhawaninayak25 <bhawaninayak1111@gmail.com>
2026-01-22 15:17:02 +0530

    rec

---
+++ day11/object.js
@@ -0,0 +1,21 @@
+// // const user={
+// //     name: "ram",
+// //     age: 22,
+// //     class:12,
+// //     greet : function(){
+// //         console.log("hello")
+// //     }  
+// // // }
+// // user.greet();
+
+
+// const user={
+//     name:"rohit",
+//     age:30,
+
+
+// }
+
+
+
+ 
---
+++ day11/recursion/app.js
@@ -0,0 +1,13 @@
+function greet(){
+    console.log("good morning ")
+}
+function enjo(){
+    console.log("enjoy fun")
+}
+function temp(){
+    console.log("track locatio temp")
+    
+}
+greet();
+enjo();
+temp();
