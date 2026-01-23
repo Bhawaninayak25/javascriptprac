6bbb7f538e75d0e3e01f38781aebfa6ea4bb9f59
Bhawaninayak25 <bhawaninayak1111@gmail.com>
2026-01-23 22:36:57 +0530

    d

--- day11/intjs.js
+++ day11/intjs.js
@@ -6,3 +6,24 @@
 // const arr = [1, 2, 3];
 // const newArr = arr.map(x => x * 2);
 // console.log(newArr);
+
+
+
+// function temp(n){
+//     if(n==0)return
+//     console.log("hello");
+//     temp(n-1)// n-- ke case m  {staack overflow }
+    
+// }  
+// temp(5)
+ 
+
+// function t(n){
+//     if(n==0)return
+//      t(n-1)
+//     console.log(n)
+   
+
+// }
+// t(5)
+
