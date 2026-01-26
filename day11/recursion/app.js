6112bceb501168afb22b602a48c3802a5da4d4af
Bhawaninayak25 <bhawaninayak1111@gmail.com>
2026-01-26 21:49:13 +0530

    d

--- day11/recursion/app.js
+++ day11/recursion/app.js
@@ -1,13 +1,26 @@
-function greet(){
-    console.log("good morning ")
-}
-function enjo(){
-    console.log("enjoy fun")
-}
-function temp(){
-    console.log("track locatio temp")
+// function greet(){
+//     console.log("good morning ")
+// }
+// function enjo(){
+//     console.log("enjoy fun")
+// }
+// function temp(){
+//     console.log("track locatio temp")
     
+// }
+// greet();
+// enjo();
+// temp();
+
+let n=10;
+let first=0, second=1;
+console.log(first)
+console.log(second)
+
+for(i=1; i<=n-2; i++){
+     third=first+second;
+     first=second;
+     second=third;
+
+    console.log(third)
 }
-greet();
-enjo();
-temp();
